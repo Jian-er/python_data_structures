393f6ce2a632937269499522ced3f7f0da25af5d
jianer <814762757@qq.com>
2020-03-11 14:57:50 +0800

    快速排序

--- DS3-sortandsearch/sortPractise.py
+++ DS3-sortandsearch/sortPractise.py
@@ -142,3 +142,52 @@
 # print(alist)
 
 
+
+'''
+    快速排序
+'''
+def quickSort(alist):
+    quickSortHelper(alist,0,len(alist)-1)
+
+# 递归调用对数列进行分区
+def quickSortHelper(alist,first,last):
+    if first<last:
+        splitpoint = partition(alist,first,last)
+
+        quickSortHelper(alist,first,splitpoint-1)
+        quickSortHelper(alist,splitpoint+1,last)
+
+# 选出基准点
+def partition(alist,first,last):
+    # 定义基准点
+    pivotvalue = alist[first]
+
+    leftMark = first + 1
+    rightMark = last
+
+    # 停止比对
+    done = False
+    while not done:
+        while leftMark <= rightMark and alist[leftMark] <= pivotvalue:
+            leftMark = leftMark + 1
+
+        while alist[rightMark] >= pivotvalue and rightMark >= leftMark:
+            rightMark = rightMark - 1
+
+        if rightMark < leftMark:
+            done = True
+        else:
+            temp = alist[leftMark]
+            alist[leftMark] = alist[rightMark]
+            alist[rightMark] = temp
+
+    temp = alist[first]
+    alist[first] = alist[rightMark]
+    alist[rightMark] = temp
+
+    return rightMark
+
+
+alist =  [54,26,93,17,77,31,44,55,20]
+quickSort(alist)
+print(alist)
